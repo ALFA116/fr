b0dafd9ecaa9b607e834396faa2b56deac96edf0
ALFA <160485300+ALFA116@users.noreply.github.com>
2024-09-17 11:20:30 +0700

    add file

---
+++ FR/pages/index.php
@@ -0,0 +1,494 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>FR MAGANG SMKN64 WEB</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <div class="home">
+    <div class="container-5">
+      <div class="group-152">
+        <div class="container-4">
+          <div class="group-117">
+            <div class="our-journey">
+              OUR JOURNEY
+            </div>
+            <span class="reason-to-chose-our-solution-already-colaboration-with-may-clients">
+              Reason to chose Our solution | Already colaboration with may clients
+            </span>
+          </div>
+          <div class="group-367">
+            <div class="group-141">
+              <div class="container">
+                <div class="image-4">
+                </div>
+                <div class="image-5">
+                </div>
+                <div class="image-6">
+                </div>
+                <div class="image-7">
+                </div>
+                <div class="image-8">
+                </div>
+              </div>
+              <div class="container-10">
+                <span class="jakarta">
+                  JAKARTA
+                </span>
+                <span class="bekasi">
+                  BEKASI
+                </span>
+                <span class="depok">
+                  DEPOK
+                </span>
+                <span class="bogor">
+                  BOGOR
+                </span>
+                <span class="bandung">
+                  BANDUNG
+                </span>
+              </div>
+            </div>
+            <div class="group-104">
+              <div class="container-7">
+                <div class="image-10">
+                </div>
+                <div class="image-11">
+                </div>
+                <div class="image-12">
+                </div>
+                <div class="image-13">
+                </div>
+                <div class="image-14">
+                </div>
+              </div>
+              <div class="container-3">
+                <span class="karawang">
+                  KARAWANG
+                </span>
+                <span class="yogyakarta">
+                  YOGYAKARTA
+                </span>
+                <span class="solo">
+                  SOLO
+                </span>
+                <span class="surabaya">
+                  SURABAYA
+                </span>
+                <span class="malang">
+                  MALANG
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="frame-122">
+          <div class="frame-121">
+            <div class="frame-118">
+              <div class="frame-117">
+                <div class="frame-15">
+                  <img class="vector-16" src="../assets/vectors/vector_5_x2.svg" />
+                </div>
+                <div class="it-telecom-savvy-team">
+                  IT &amp; TELECOM-Savvy Team
+                </div>
+              </div>
+              <span class="we-are-experienced-professionals-that-have-served-it-and-telecom-industries-for-more-than-twelve-years-we-bring-acombination-of-technical-and-market-knowledge-to-each-of-our-projects">
+                We are experienced professionals that have served IT and telecom industries for more than twelve years. We bring a combination of technical and market knowledge to each of our projects.
+              </span>
+            </div>
+            <div class="frame-119">
+              <div class="frame-1171">
+                <div class="frame-16">
+                  <img class="vector-17" src="../assets/vectors/vector_19_x2.svg" />
+                </div>
+                <div class="collaboration">
+                  Halo Collaboration
+                </div>
+              </div>
+              <span class="collaborate-with-multinational-companies-which-have-expertise-in-it-solution-and-telecom-carriers-we-tailor-and-match-customers-needs-with-effective-and-efficient-solution">
+                Collaborate with multinational companies, which have expertise in IT solution and telecom carriers, we tailor and match customers needs with effective and efficient solution.
+              </span>
+            </div>
+            <div class="frame-120">
+              <div class="frame-1172">
+                <div class="frame-17">
+                  <img class="vector-18" src="../assets/vectors/vector_2_x2.svg" />
+                </div>
+                <div class="value-to-costomers">
+                  Value to costomers
+                </div>
+              </div>
+              <span class="by-focusing-solely-on-areas-where-we-excel-we-aim-to-provide-afirst-class-service-to-our-customers-and-bring-value-for-our-customers-by-producing-measurable-results-and-cost-savings">
+                By focusing solely on areas where we excel, we aim to provide a first-class service to our customers and bring value for our customers by producing measurable results and cost savings.
+              </span>
+            </div>
+          </div>
+          <div class="group-114">
+            <span class="about-us-1">
+              About us
+            </span>
+          </div>
+        </div>
+        <div class="group-116">
+          <span class="reason-to-chose-our-solution-already-colaboration-with-may-clients-1">
+            Reason to chose Our solution | Already colaboration with may clients
+          </span>
+        </div>
+      </div>
+      <div class="frame-154">
+        <div class="frame-144">
+          <div class="frame-140">
+            <div class="our-contact">
+              OUR CONTACT
+            </div>
+            <p class="get-schedule-with-us">
+              <span class="get-schedule-with-us-sub-3"></span><span></span>
+            </p>
+          </div>
+          <div class="frame-143">
+            <div class="frame-141">
+              <div class="group-365">
+                <div class="frame-6">
+                  <img class="vector-6" src="../assets/vectors/vector_17_x2.svg" />
+                </div>
+              </div>
+              <span class="jl-guntur-no-20-climo-kec-limo-kota-depok-jawa-barat-16514">
+                Jl. Guntur No.20c, Limo, kec. Limo,<br />
+                Kota depok, jawa barat 16514
+              </span>
+            </div>
+            <div class="frame-142">
+              <div class="group-366">
+                <div class="frame-7">
+                  <img class="vector-7" src="../assets/vectors/vector_3_x2.svg" />
+                </div>
+              </div>
+              <div class="sales-representative">
+                Sales Representative
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="frame-153">
+          <div class="frame-150">
+            <div class="frame-149">
+              <div class="frame-8">
+                <img class="vector-9" src="../assets/vectors/vector_x2.svg" />
+              </div>
+              <p class="leave-amessage">
+                <span class="leave-amessage-sub-5"></span><span></span>
+              </p>
+            </div>
+            <div class="frame-148">
+              <div class="frame-146">
+                <div class="name">
+                  NAME
+                </div>
+                <div class="frame-145">
+                  <span class="full-name">
+                    Full Name
+                  </span>
+                </div>
+              </div>
+              <div class="frame-147">
+                <div class="email">
+                  Email
+                </div>
+                <div class="frame-1451">
+                  <span class="enter-your-email">
+                    Enter Your Email
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="frame-1481">
+              <div class="your-message">
+                Your Message
+              </div>
+              <div class="frame-1452">
+                <span class="enter-your-message">
+                  Enter Your Message . . . .
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="frame-152">
+            <span class="send-now">
+              Send Now
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="frame-164">
+        <div class="image-91">
+        </div>
+        <div class="frame-162">
+          <div class="we-plavide-arange-of-it-and-lelucomunication-services-conseling-of-telecomm-consultancy-sip-trunk-provider-cell-center-solution-lan-setup-we-do-the-concept-technical-design-and-implematation-technologika-costumer-need-bath-attective-cast-efficients">
+            we plavide a range of IT and lelucomunication services conseling of telecomm consultancy SIP Trunk provider, cell center Solution, Lan setup<br />
+            we do the concept, technical design, and implematation technologika costumer need bath attective cast-efficients.
+          </div>
+          <div class="frame-163">
+            <div class="frame-157">
+              <p class="service">
+                <span class="service-sub-6"></span><span></span>
+              </p>
+              <div class="frame-156">
+                <span class="our-work">
+                  Our work
+                </span>
+                <span class="our-solutions">
+                  Our solutions
+                </span>
+              </div>
+            </div>
+            <div class="frame-159">
+              <div class="about-us">
+                About us
+              </div>
+              <div class="frame-158">
+                <div class="overview">
+                  overview
+                </div>
+                <div class="aboue">
+                  Aboue
+                </div>
+                <div class="awards-recagritions">
+                  Awards &amp; Recagritions
+                </div>
+                <span class="contact-team">
+                  Contact Team
+                </span>
+              </div>
+            </div>
+            <div class="frame-161">
+              <div class="pt-meissa-berkah-teknologi">
+                PT.Meissa Berkah Teknologi
+              </div>
+              <div class="frame-160">
+                <div class="jl-guntur-no-20-climo-kec-limo-kota-depok-jawa-barat-16614">
+                  JL. Guntur No20C, Limo, kec Limo. Kota Depok Jawa barat 16614
+                </div>
+                <span class="container-4">
+                  +5285972510714
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="frame-171">
+        <div class="copyright-2024-first-routes-web-team-all-rights-reserved">
+          Copyright@2024. First-Routes Web Team All Rights Reserved.
+        </div>
+        <div class="frame-170">
+          <div class="frame-169">
+            <div class="frame-10">
+              <img class="vector-11" src="../assets/vectors/vector_8_x2.svg" />
+            </div>
+            <div class="container-2">
+              <div class="frame-11">
+                <img class="vector-12" src="../assets/vectors/vector_9_x2.svg" />
+              </div>
+              <div class="frame-12">
+                <img class="vector-13" src="../assets/vectors/vector_15_x2.svg" />
+              </div>
+              <div class="frame-13">
+                <img class="vector-14" src="../assets/vectors/vector_11_x2.svg" />
+              </div>
+            </div>
+          </div>
+          <div class="akun-media-sosial-kami">
+            Akun media sosial kami
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-1">
+      <div class="container-8">
+        <div class="image-9">
+        </div>
+        <div class="container-6">
+          <div class="home-1 navbar-item">Home</div>
+          <div class="about navbar-item">About</div>
+          <div class="work navbar-item">Work</div>
+          <div class="product-solutions navbar-item">Product &amp; Solutions</div>
+          <div class="magang navbar-item">Magang</div>
+          <div class="contact navbar-item">Contact</div>
+        </div>
+      </div>
+      <span class="your-consultancy-partner-for-growth">
+        Your Consultancy</br>
+        Partner for Growth.
+      </span>
+      <div class="find-solutions-and-work-fast-implementing-idea-let-us-manage-your-it-infrastructure-stop-doing-works-that-arent-your-core-businesses">
+        Find Solutions and Work Fast implementing Idea. Let us</br>
+        Manage Your IT Infrastructure Stop doing works that aren&#39;t</br>
+        your core businesses.
+      </div>
+      <div class="group-88">
+        <div class="contohgmail-com">
+          Contoh@gmail.com
+        </div>
+        <div class="group-86">
+          <span class="email-1">
+            Email
+          </span>
+        </div>
+      </div>
+      <div class="container-9">
+        <div class="group-83">
+          <img class="group-81" src="../assets/vectors/group_81_x2.svg" />
+          <img class="group-82" src="../assets/vectors/group_82_x2.svg" />
+        </div>
+        <div class="group-84">
+          <div class="frame-14">
+            <img class="vector-15" src="../assets/vectors/vector_4_x2.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="frame-139">
+      <div class="frame-133">
+        <div class="frame-132">
+          <div class="work-with-us">
+            work with us
+          </div>
+          <div class="collaboration-for-new-invention">
+            Collaboration <br />
+            for New Invention
+          </div>
+          <span class="what-we-do-pt-meissa-berkah-teknologi-is-aspecialist-company-that-delivers-telecommunication-services-under-the-brand-first-routes-the-company-is-established-in-2014-by-ateam-of-energetic-and-experienced-telecoms-professionals-that-aim-to-deliver-effective-and-efficient-telecommunication-services">
+            What We Do<br />
+            PT. Meissa Berkah Teknologi is a specialist company that delivers telecommunication services under the brand First Routes. The company is established in 2014 by a team of energetic and experienced telecoms professionals that aim to deliver effective and efficient telecommunication services.
+          </span>
+        </div>
+        <div class="frame-131">
+          <div class="frame-129">
+            <div class="frame-127">
+              <div class="image-15">
+              </div>
+              <div class="frame-125">
+                <div class="best-service">
+                  [ Best Service ]
+                </div>
+                <div class="frame-124">
+                  <div class="frame-18">
+                    <img class="vector-19" src="../assets/vectors/vector_21_x2.svg" />
+                    <div class="frame-19">
+                      <img class="vector-20" src="../assets/vectors/vector_12_x2.svg" />
+                    </div>
+                  </div>
+                  <div class="frame-20">
+                    <img class="vector-21" src="../assets/vectors/vector_16_x2.svg" />
+                    <div class="frame-21">
+                      <img class="vector-22" src="../assets/vectors/vector_18_x2.svg" />
+                    </div>
+                  </div>
+                  <div class="frame-22">
+                    <img class="vector-23" src="../assets/vectors/vector_14_x2.svg" />
+                    <div class="frame-23">
+                      <img class="vector" src="../assets/vectors/vector_20_x2.svg" />
+                    </div>
+                  </div>
+                  <div class="frame">
+                    <img class="vector-1" src="../assets/vectors/vector_7_x2.svg" />
+                    <div class="frame-1">
+                      <img class="vector-2" src="../assets/vectors/vector_1_x2.svg" />
+                    </div>
+                  </div>
+                  <div class="frame-2">
+                    <img class="vector-3" src="../assets/vectors/vector_13_x2.svg" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="frame-126">
+              <span class="ui-ux-design">
+                UI/UX Design
+              </span>
+              <span class="antarmuka-sangat-intuitif-dan-mudah-dipahami-oleh-pengguna-baru-tugas-tugas-utama-dapat-diselesaikan-dengan-cepat-dan-efisien">
+                Antarmuka sangat intuitif dan mudah dipahami oleh pengguna baru.Tugas-tugas utama dapat diselesaikan dengan cepat dan efisien.
+              </span>
+            </div>
+          </div>
+          <div class="frame-130">
+            <div class="group-353">
+              <div class="frame-3">
+                <img class="vector-4" src="../assets/vectors/vector_6_x2.svg" />
+              </div>
+            </div>
+            <div class="group-354">
+              <div class="frame-4">
+                <img class="vector-5" src="../assets/vectors/vector_10_x2.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="frame-137">
+        <div class="frame-135">
+          <div class="image">
+          </div>
+          <div class="frame-134">
+            <span class="container">
+              12+
+            </span>
+            <span class="telco-carriers-and-isp-partners">
+              Telco Carriers and ISP Partners
+            </span>
+          </div>
+        </div>
+        <div class="frame-136">
+          <div class="image-1">
+          </div>
+          <div class="frame-1341">
+            <span class="container-1">
+              120+
+            </span>
+            <span class="project-done">
+              Project Done
+            </span>
+          </div>
+        </div>
+        <div class="frame-1371">
+          <div class="image-2">
+          </div>
+          <div class="frame-1342">
+            <span class="container-2">
+              90+
+            </span>
+            <span class="corporate-clients">
+              Corporate Clients
+            </span>
+          </div>
+        </div>
+        <div class="frame-138">
+          <div class="image-3">
+          </div>
+          <div class="frame-1343">
+            <span class="container-3">
+              0+
+            </span>
+            <span class="pending-project">
+              Pending Project
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
